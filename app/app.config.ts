f742cd6fba0e2ca15518feb30190279aae1b977f
I'm OWEN <146957570+oowenge@users.noreply.github.com>
2025-07-13 04:10:26 +0000

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
   title: '潮廷短链生成器',
   email: 'ctowen@duck.com',
-  github: 'https://t.me/owenwork',
-  twitter: 'https://t.me/owenwork',
-  telegram: 'https://t.me/owenwork',
-  mastodon: 'https://t.me/owenwork',
-  blog: 'https://t.me/owenwork',
+  github: 'https://cturl.dpdns.org/',
+  twitter: 'https://cturl.dpdns.org/',
+  telegram: 'https://cturl.dpdns.org/',
+  mastodon: 'https://cturl.dpdns.org/',
+  blog: 'https://cturl.dpdns.org/',
   description: '简单/快速/安全的链接缩短器，带分析功能。',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
