2ef8c0ff022d672196304a7970fb11c1d77700a9
I'm OWEN <146957570+oowenge@users.noreply.github.com>
2025-07-13 03:36:16 +0000

    Update Footer.vue

--- app/components/layouts/Footer.vue
+++ app/components/layouts/Footer.vue
@@ -8,17 +8,17 @@ const { title, email, telegram, blog, twitter, mastodon, github } = useAppConfig
   <section class="md:pt-6">
     <div class="container flex flex-col items-center py-8 mx-auto sm:flex-row">
       <a
-        href="https://sink.cool"
+        href="https://cturl.dpdns.org"
         class="text-xl font-black leading-none text-gray-900 select-none dark:text-gray-100 logo"
         :title="title"
       >{{ title }}</a>
       <a
         class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0"
-        href="https://html.zone"
+        href="https://cturl.dpdns.org"
         target="_blank"
         title="HTML.ZONE"
       >
-        &copy; {{ new Date().getFullYear() }} Products of HTML.ZONE
+        &copy; {{ new Date().getFullYear() }} Products of OWEN
       </a>
       <span
         class="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start"
