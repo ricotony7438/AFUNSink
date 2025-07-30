17171d520b36de58fcbfb9614bb509df7bc1c30f
ricotony7438 <ricotony7438@gmail.com>
2025-07-30 21:55:44 +0000

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,12 +1,12 @@
 export default defineAppConfig({
-  title: '潮廷AFUN短链生成器',
+  title: '潮廷短链中心',
   email: 'ctowen@duck.com',
   github: 'https://afun.center/',
   twitter: 'https://afun.center/',
   telegram: 'https://afun.center/',
   mastodon: 'https://afun.center/',
   blog: 'https://afun.center/',
-  description: '简单/快速/安全的链接缩短器，带分析功能。',
+  description: '简单/快速/安全的链接缩短器，带全球分析功能。',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
