543adaf2c1e694f6f22798bf9e4887d510de39c2
ricotony7438 <ricotony7438@gmail.com>
2025-07-30 21:44:44 +0000

    Update Footer.vue

--- app/components/layouts/Footer.vue
+++ app/components/layouts/Footer.vue
@@ -8,13 +8,13 @@ const { title, email, telegram, blog, twitter, mastodon, github } = useAppConfig
   <section class="md:pt-6">
     <div class="container flex flex-col items-center py-8 mx-auto sm:flex-row">
       <a
-        href="https://cturl.dpdns.org"
+        href="https://afun.center/"
         class="text-xl font-black leading-none text-gray-900 select-none dark:text-gray-100 logo"
         :title="title"
       >{{ title }}</a>
       <a
         class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0"
-        href="https://cturl.dpdns.org"
+        href="https://afun.center/"
         target="_blank"
         title="HTML.ZONE"
       >
