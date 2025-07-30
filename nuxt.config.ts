1153387aa0e8e2be369f096ad3e314d5143f13c0
ricotony7438 <ricotony7438@gmail.com>
2025-07-30 17:12:07 -0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -122,16 +122,9 @@ export default defineNuxtConfig({
   },
 
   shadcn: {
-    /**
-     * Prefix for all the imported component
-     */
     prefix: '',
-    /**
-     * Directory that the component lives in.
-     * @default "./components/ui"
-     */
     componentDir: './app/components/ui',
   },
 
-  plugins: ['~/app/plugins/buffer-polyfill.ts'],
+  plugins: ['~/plugins/buffer-polyfill.ts'],
 })
